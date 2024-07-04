2aef8424806ce6cf9485956c259b2962eef8aa13
vinod kumar <36531617+shilpathota@users.noreply.github.com>
2024-07-04 06:42:49 -0500

    update

--- src/components/Link.jsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import { Link as RrdLink, LinkProps } from "react-router-dom";
-
-const Link = ({ href, ...args }) => {
-  return <RrdLink {...args} to={href} reloadDocument={true}/>;
-};
-
-export default Link;
--- src/components/listing-image-gallery/utils/types.js
+++
@@ -1,4 +0,0 @@
-export type ListingGalleryImage={
-    id:number;
-    url:string;
-}
