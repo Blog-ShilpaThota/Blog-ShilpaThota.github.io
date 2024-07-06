0712971f3479751bd44cf6b0b9fde8b6ea7bbab3
vinod kumar <36531617+shilpathota@users.noreply.github.com>
2024-07-06 18:30:58 -0500

    Update PostMediumArticles.jsx

--- src/app/page/PostMediumArticles.jsx
+++ src/app/page/PostMediumArticles.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import NcImage from "../../components/NcImage/NcImage";
 import SingleHeaderMediumArticle from "../singles/SingleHeaderMediumArticle";
 import Layout from "./Postlayout";
 import { useParams } from "react-router-dom";
